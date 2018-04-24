4fecfd7f19b0d861ad2fbc2d57867a645388e978
61_lsc <>
2018-04-24 15:01:01 +0800

    1

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         _ = "aaaa"
+        let d = 1
     }
 
     override func didReceiveMemoryWarning() {
