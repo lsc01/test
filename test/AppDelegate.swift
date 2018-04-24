e8208b4e2992663d0708f13f19d39d0a9d8808e7
61_lsc <>
2018-04-24 15:37:29 +0800

    LSC2

--- test/AppDelegate.swift
+++ test/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //这里改一下
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
